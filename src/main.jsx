9afc0ce81d284531e94be44438959db4fe19d0a0
Lázaro <lazaroyaesta@gmail.com>
2022-07-25 19:04:12 +0200

    ✨ Configure Apollo Client to use Subscriptions

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,10 @@ import {
   InMemoryCache,
   createHttpLink,
   setContenxt,
+  split,
 } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
@@ -27,9 +30,29 @@ const authLink = setContenxt((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: splitLink,
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
